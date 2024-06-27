9dd6ccafd00bfc661a47792c388950baa5a7abb7
Sayali <sayaligaikwad.usa@gmail.com>
2024-06-27 14:22:50 -0400

    Added Image UI

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import MyText from './component/MyText';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, Image} from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <MyText text={'Hello, Sayali here form props'} />
+      <MyText text={'Hello User, welcome !'} />
+      <Image
+        source={require('./assets/images/cake.png')}
+        style={{width: '100%', height: '100%', backgroundColor: 'beige'}}
+        resizeMode={'center'}
+      />
     </SafeAreaView>
   );
 };
